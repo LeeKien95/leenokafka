69e82831973402fee492dc1b7349bd24032c0eff
LeeKien95 <kienletrung.1995@gmail.com>
2023-01-04 13:54:45 +0900

    first commit

---
+++ src/main/java/leenosleep/leenokafka/App.java
@@ -0,0 +1,19 @@
+package leenosleep.leenokafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@SpringBootApplication
+class App {
+
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
+
+}
